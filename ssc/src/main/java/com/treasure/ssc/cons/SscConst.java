719da01f39124c54b43644dcd424aaa2fe0e8a7e
mazhaoyi <690196003@qq.com>
2018-09-19 17:10:35 +0800

    const

--- ssc/src/main/java/com/treasure/ssc/cons/SscConst.java
+++ ssc/src/main/java/com/treasure/ssc/cons/SscConst.java
@@ -6,13 +6,4 @@ package com.treasure.ssc.cons;
  */
 public class SscConst {
     public static final String DATA_URL = "http://39.108.143.25:8080/stock/cqssc/getCurrentDay.sc";
-
-    /**
-     * num类型
-     */
-    enum NumTypeEnum{
-        BEF_3,
-        AFT_3,
-        MID_3
-    }
 }
