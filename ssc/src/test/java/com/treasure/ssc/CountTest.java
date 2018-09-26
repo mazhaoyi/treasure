a16719ec43a93f044f67729edf0da3d18de87f48
mazhaoyi <690196003@qq.com>
2018-09-26 21:52:59 +0800

    1

---
+++ ssc/src/test/java/com/treasure/ssc/CountTest.java
@@ -0,0 +1,14 @@
+package com.treasure.ssc;
+
+import org.junit.Test;
+
+/**
+ * @author: mazy
+ * @date: 2018/9/26 21:51
+ */
+public class CountTest {
+    @Test
+    public void count() {
+
+    }
+}
